3ebb62947785aadebb07d809e6bb47642fa42af2
Tyler Woodcook <twoodcook@gmail.com>
2019-07-19 17:06:03 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,24 @@
-Javascript
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script>
+  $(document).ready(function(){
+
+	$(".dropdown-content").hide();
+	
+	$(".dropdown-button").click(function(){
+		$(".dropdown-content").toggle(300);
+	})
+	
+	$("#hero-button").mouseenter(function(){
+  		$("#hero-button").animate({
+    		width: '+=20px'
+		}, 200)
+	})
+  
+	$("#hero-button").mouseleave(function(){
+  		$("#hero-button").animate({
+    		width: '-=20px'
+		}, 200)
+	})
+  
+  });
+</script>
